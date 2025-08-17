a2ec813f2607c18a2c7c8b40fa41c6ef1f139e3f
gwak <git@momohyphon.com>
2025-08-17 19:32:36 +0900

    여기에 변경 내용을 간결하게 적어주세요

---
+++ 3_declaring_variable.js
@@ -0,0 +1,46 @@
+
+/**
+ * 
+ * Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let
+ * 3) const
+ * 
+ */
+var name = '코드팩토리'; //따옴표를 쓰면 안됨.
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경 가능하다.
+ * 
+ */
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = '코드팩토리';
+console.log(newJeans)
+
+/**
+ * 선언과 할당
+ * 
+ * 1)변수를 선언 하는 것.
+ * 2)할당
+ */
+var name = '코드팩토리';
+console.log(name);
+
+let girlFriend;
+console.log(girlFriend);
+
+const girlFriend2;
