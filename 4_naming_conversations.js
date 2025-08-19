a7d3b35a2bb7e6fc7c994efff27b7cb81d8dacb8
momohyphon <kjb690@gmail.com>
2025-08-19 23:29:53 +0900

    새항목

---
+++ 4_naming_conversations.js
@@ -0,0 +1,35 @@
+/**
+ * Naming Conversations
+ * 
+ * 1) 일반적으로 영어(라틴문자)를 사용하여 문자와 숫자를 모두 사용할 수 있다.
+ * 2) 특수기호는 언더스코어와 달러를 사용할 수 없다.
+ * 3) 숫자로 이름을 시작 할 수 없다.
+ *    1name, 2hello
+ * 4) 키워드는 변수명으로 사용할 수 없다.
+ *    var const = 'var';
+ *  
+ */
+let codefactory = '코드팩토리';
+var $ive ='아이브';
+const _yujin = '안유진';
+
+console.log(codefactory);
+console.log($ive);
+console.log(_yujin);
+
+/**
+ * let 1name = 'no';
+ * let const = 'attention';
+ */
+
+/**
+ * Naming Conversation 2
+ * 1) camelCase - 대부분의 언어에서 많이 사용
+ * 2) sanke_case -> 파이선 같은 언어에 사용
+ * 3) PascalCase -> C# 마이클소프트 계열의 사용함.
+ * 
+ */
+
+//안유진
+const anYujin = '안유진';
+console.log(anYujin);
