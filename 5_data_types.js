8a569071d9350dee49b1f6effdfb7d30196cdf01
momohyphon <kjb690@gmail.com>
2025-08-25 00:47:26 +0900

    5번 추가

---
+++ 5_data_types.js
@@ -0,0 +1,46 @@
+/**
+ * Date Types
+ * 
+ * 여섯개의 Primitive Type와
+ * 한개의 오브젝트 타입이있다.
+ * 
+ * 1) Number (숫자)
+ * 2) String (문자열)
+ * 3) Boolean (불리언)
+ * 4) Undefine (언디파인드)
+ * 5) null (널)
+ * 6) Symbol (심볼)
+ * 
+ * 7) Object (객체)
+ *    Function
+ *    Array
+ *    Object
+ */
+
+//number Type -------------
+const age = 32;
+const temperature = -10;
+const pi = 3.14;
+
+console.log(typeof age);
+console.log(typeof temperature);
+console.log(typeof pi);
+
+
+const infinity = Infinity;
+const ninfinity = -Infinity;
+
+console.log(typeof infinity);
+console.log(typeof ninfinity);
+console.log('-------------------');
+
+/**
+ *  String Type
+ */
+
+const codeFactory = '코드팩토리';
+console.log(typeof codeFactory);
+
+const ive = "'아이브' 안유진";
+console.log(ive)
+
