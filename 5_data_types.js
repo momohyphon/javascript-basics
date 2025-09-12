bd07cd24ed4e25d9c885b808852ad1a54b4b02b8
gwak <git@momohyphon.com>
2025-09-12 15:57:53 +0900

    5번추가2

--- 5_data_types.js
+++ 5_data_types.js
@@ -112,6 +112,71 @@ console.log(typeof init);
  * 호출해서 사용한다.
  */
 const test1 = '1';
-const test2 = '2';
+const test2 = '1';
 
 console.log(test1 === test2);
+
+const symbol1 = Symbol('1');
+const symbol2 = Symbol('1');
+
+console.log(symbol1 === symbol2);
+
+
+console.log('----------------------');
+
+/**
+ * Object type
+ * 
+ * Map
+ * 키:밸류의 값으로 이루어진다.
+ * Key:value
+ */
+const dictionary = {
+  red: '빨간색',
+  orange: '주황색',
+  yellow: '노란색',
+};
+console.log(dictionary);
+console.log(dictionary['red']);
+console.log(dictionary['orange']);
+console.log(dictionary['yellow']);
+
+console.log(typeof dictionary);
+
+/**
+ * Array type
+ * 
+ * 값을 리스트로 나열할 수 있는 타입이다.
+ * 
+ */
+
+const iveMembersArray = [
+  '안유진',
+  '가을',
+  '레이',
+  '장원영',
+  '리즈',
+  '이시',
+];
+console.log(iveMembersArray);
+
+/**
+ * Index
+ * 
+ * 0부터 시작한다.
+ * 1씩 올라간다
+ */
+
+console.log(iveMembersArray[0]);
+console.log(iveMembersArray[5]);
+iveMembersArray[0] = '코드팩토리'
+console.log(iveMembersArray);
+console.log(typeof iveMembersArray);
+
+/**
+ * static typing --> 변수를 선언할때 어떤 타입의 변수를 선언할지 명시를 한다.
+ * 
+ * dynamic typing --> 변수의 타입을 선언하지 않고 값에 의해 타입을 "추론"한다.
+ * JS --> dynamic typing
+ */
+
