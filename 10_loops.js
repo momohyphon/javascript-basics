ec8f5e3dba670aef7252cc0ad23288fdfd767507
gwak <git@momohyphon.com>
2025-10-29 18:59:06 +0900

    study3

---
+++ 10_loops.js
@@ -0,0 +1,15 @@
+/**
+ * Loops
+ * 1) for
+ * 2) while
+ */
+
+for (let i = 0; i <10; i ++) {
+    console.log(i);
+}
+
+console.log('---------------------');
+for(let i = 10; i > 0; i --){
+    console.log(i)
+}
+
